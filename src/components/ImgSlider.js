be55f3d00ad6fa269432e13b3c5d70351595cee0
Vipul Chaudhary <vipschaudhary044@gmail.com>
2023-03-13 18:56:49 +0530

    Added sliders  from slick and slick-Carousel pckg

---
+++ src/components/ImgSlider.js
@@ -0,0 +1,77 @@
+import styled from "styled-components";
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+
+const ImgSlider = () => {
+  let settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+  };
+
+  return (
+    <Carousel {...settings}>
+      <div>
+        <h1>Hello1</h1>
+      </div>
+      <div>
+        <h1>Hello2</h1>
+      </div>
+      <div>
+        <h1>Hello3</h1>
+      </div>
+      <div>
+        <h1>Hello4</h1>
+      </div>
+      <div>
+        <h1>Hello5</h1>
+      </div>
+    </Carousel>
+  );
+};
+
+export default ImgSlider;
+
+const Carousel = styled(Slider)`
+  margin-top: 20px;
+
+  & > button {
+    opacity: 0;
+    height: 100%;
+    width: 5vw;
+    z-index: 1;
+
+    &:hover {
+      opacity: 1;
+      transition: opacity 0.2s ease 0s;
+    }
+  }
+
+  ul li button {
+    &:before {
+      font-size: 10px;
+      color: rgb(150, 158, 171);
+    }
+  }
+
+  li.slick-active button:before {
+    color: white;
+  }
+
+  .slick-list {
+    overflow: initial;
+  }
+
+  .slick-prev {
+    left: -75px;
+  }
+
+  .slick-next {
+    right: -75px;
+  }
+`;
